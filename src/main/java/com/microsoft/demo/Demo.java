5ac628324ada21a9b7dd7300af6f94aa65a70f98
Diego Armando Gomez Mosquera <dgomez@Diegos-MacBook-Pro.local>
2021-01-11 16:51:57 -0500

    Prueba

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,7 +3,7 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered Johan");
+            System.out.println("I am covered");
             return;
         }
 
